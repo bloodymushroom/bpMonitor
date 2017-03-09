1d06d53ef09baa0095a3174d7a165f33bf407695
Emmeline <Emmeline.Lan@gmail.com>
2017-03-08 22:37:25 -0800

    added mobx state management

--- src/js/mobx/Store.js
+++ src/js/mobx/Store.js
@@ -1,11 +1,26 @@
 import { observable, action } from 'mobx'
 
 class Store {
+  context = this;
+  canvas = null;
   @observable user = 'test';
+  @observable dayRange = '30';
 
   @action updateUser(user) {
     this.user = user;
   }
+
+  @action addReading(arr) {
+    this.allData.push(arr);
+  }
+
+  allData = Array.from( new Array(600), (x, index) => {
+    var sys = Math.floor(Math.random() * 10) + 120;
+    var dia = Math.floor(Math.random() * 10) + 80;
+    return [sys, dia, index]
+  })
+
+  @observable currentIndex = this.allData.length;
 }
 
 const store = new Store();
