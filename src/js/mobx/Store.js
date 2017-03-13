a24c7efc31799bc4057b55117070353df7dfd74a
Emmeline <Emmeline.Lan@gmail.com>
2017-03-12 21:13:32 -0700

    changed actions to be compatible with auth0

--- src/js/mobx/Store.js
+++ src/js/mobx/Store.js
@@ -16,19 +16,28 @@ class Store {
   @observable user = null;
   @observable accessToken = null;
   @observable userId = null;
+  @observable loggedIn = false;
+
   @observable isAuthenticated = false;
   @observable dayRange = 30;
   @observable loginStatus = null;
   @observable allData = [];
   @observable dataLoaded = false;
 
-
+  @action resetAll() {
+    this.user = null;
+    this.accessToken = null;
+    this.userId = null;
+    this.loggedIn = false;
+    this.loginStatus = null;
+    this.allData = [];
+  }
   // auth0
   @action initializeAuth0() {
+    console.log('no token');
     var context = this;
-    this.lock = new Auth0Lock(secrets.CLIENT_ID, secrets.DOMAIN);
+    this.lock = new Auth0Lock(secrets.CLIENT_ID, secrets.DOMAIN, {redirect: false});
     this.lock.on("authenticated", function(authResult) {
-      console.log('this', this)
       context.lock.getUserInfo(authResult.accessToken, function(error, profile) {
         if (error) {
           // Handle error
@@ -36,30 +45,44 @@ class Store {
         }
 
         // Save token and profile locally
+        console.log(authResult)
         localStorage.setItem("accessToken", authResult.accessToken);
         localStorage.setItem("profile", JSON.stringify(profile));
-
+        localStorage.setItem("idToken", authResult.idToken);
+        
         // Update DOM
-        this.accessToken = authResult.accessToken;
-        this.user = {
+        context.user = {
           email: profile.email,
-          username: profile.name
+          username: profile.name,
+          clientID: profile.clientID
         }
+        context.accessToken = authResult.accessToken;
+        context.idToken = authResult.idToken;
+
+        console.log('added user: ', profile.email, profile.name)
       });
     });
+
+    this.lock.show();
   }
 
   @action getAccessToken() {
     var accessToken = localStorage.getItem('accessToken');
-    var profile = localStorage.getItem('profile');
+    var profile = JSON.parse(localStorage.getItem('profile'));
+    var idToken = localStorage.getItem('idToken');
 
     if (accessToken && profile) {
       this.accessToken = accessToken;
       this.user = {
         email: profile.email,
-        username: profile.name
+        username: profile.name,
+        clientID: profile.clientID
       }
+
+      return true;
     }
+
+    return false;
   }
 
   @action getProfile() {
@@ -87,72 +110,96 @@ class Store {
   }
 
   @action startSession(res) {
+    console.log('in startsession', res)
     this.user = res.user;
-    this.userId = res.user._id
-    this.isAuthenticated = true;
+    this.loggedIn = true;
     this.allData = res.bps;
+    console.log('got user', this.user, 'got data', this.allData)
     setTimeout(() => {
       renderChart(store.canvas, this.allData.slice(-this.dayRange))      
     }, 1000)
   }
 
-  @action register(user) {
-    fetch(server + 'register', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-    .then( (res) => {
-      if (res.status !== 200) {
-        throw res.json();
-      }
-      console.log('status', res.status);
-      return res.json()
-    })
-    .then( (data) => {
-      console.log('added user!', data)
-      this.startSession(data)
-      return data;
-    })
-    .catch( (e) => {
-      e.then( (err) => {
-        console.error('didnt find user', err)
-        this.isAuthenticated = false;
-        this.loginStatus = err.message;
+  @action register() {
+    // this.lock.show();
+    if (localStorage.idToken && localStorage.profile) {
+      const profile = JSON.parse(localStorage.profile);
+      console.log(profile.clientID)
+      fetch(server + 'register', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.idToken
+        },
+        body: JSON.stringify({
+          username: profile.name,
+          email: profile.email,
+          clientID: profile.clientID
+        })
       })
-    })
-  }
-
-  @action login(user){
-    fetch(server + 'login', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-    .then( (res) => {
-      if (res.status !== 200) {
-        throw res.json();
-      }
-
-      console.log('status', res.status);
-      return res.json()
-    })
-    .then((res)  => {
-      this.startSession(res)
-
-      // this.getBP(user._id);
-    })
-    .catch(e => {
-      console.log('error before login promise:', e)
+      .then( (res) => {
+        if (res.status !== 200) {
+          throw res.json();
+        }
+        console.log('status', res.status);
+        return res.json()
+      })
+      .then( (data) => {
+        this.loggedIn = true;
+        console.log('added user!', data)
+        this.startSession(data)
+        return data;
+      })
+      .catch( (e) => {
         e.then( (err) => {
+          console.error('didnt find user', err)
           this.isAuthenticated = false;
           this.loginStatus = err.message;
         })
-    })
+      })
+    } else {
+      this.initializeAuth0();
+      this.loginStatus = 'Login failed, please try again.'
+    }
+  }
+
+  @action login() {
+    if (localStorage.idToken && localStorage.profile) {
+      const profile = JSON.parse(localStorage.profile);
+      console.log('email': localStorage.profile.email)
+      fetch(server + 'login', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.idToken
+        },
+        body: JSON.stringify({
+          email: profile.email,
+          clientID: profile.clientID
+        })
+      })
+      .then( (res) => {
+        if (res.status !== 200) {
+          throw res.json();
+        }
+
+        console.log('status', res.status);
+        return res.json()
+      })
+      .then((res)  => {
+        this.startSession(res)
+      })
+      .catch(e => {
+        console.log('error before login promise:', e)
+          e.then( (err) => {
+            this.isAuthenticated = false;
+            this.loginStatus = err.message;
+          })
+      })
+    } else {
+      console.log('login failed')
+      this.initializeAuth0();
+    }
   }
 
   @action auth0() {
@@ -172,10 +219,11 @@ class Store {
     fetch(server + 'BP', {
       method: 'post',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.idToken
       },
       body: JSON.stringify({
-        username: 'test',
+        email: this.user.email,
         bp: bp
       })
     })
